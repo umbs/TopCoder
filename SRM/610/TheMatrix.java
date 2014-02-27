3f8dba9edfe1bebed7ae7c2d20fa8afadd7ff4e4
umbs <umbs_sairam@yahoo.com>
2014-02-26 18:05:56 -0800

    Solutions to SRM problems
    Some are half baked and first stab. I’ll improve them, eventually.

---
+++ SRM/610/TheMatrix.java
@@ -0,0 +1,15 @@
+
+public class TheMatrix {
+
+	public int MaxArea(String[] b) {
+		int res = 0; 
+		
+		return res; 
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
